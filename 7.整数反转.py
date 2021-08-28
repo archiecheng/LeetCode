cba0be168272ce7a6d81d51873f51534a59a24f1
archiecheng <1804528105@qq.com>
2021-08-28 16:52:42 +0800

    Finish Question_7

---
+++ 7.整数反转.py
@@ -0,0 +1,21 @@
+'''
+Author: your name
+Date: 2021-08-28 16:45:48
+LastEditTime: 2021-08-28 16:47:13
+LastEditors: your name
+Description: In User Settings Edit
+FilePath: \LeetCode\7.整数反转.py
+'''
+#
+# @lc app=leetcode.cn id=7 lang=python3
+#
+# [7] 整数反转
+#
+
+# @lc code=start
+class Solution:
+    def reverse(self, x: int) -> int:
+        tmp = int((str(x) if x > 0 else str(-x) + "-")[::-1])
+        return tmp if -2 ** 31 < tmp < 2 ** 31 - 1 else 0
+# @lc code=end
+
